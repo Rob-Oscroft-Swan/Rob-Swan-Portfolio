09633574862cf8f7eb164698847d47b174635014
ChloeAvoChoc <97017313+ChloeAvoChoc@users.noreply.github.com>
2024-06-22 10:03:18 +0200

    Modified API Call

--- src/components/WhatsappMessage.jsx
+++ src/components/WhatsappMessage.jsx
@@ -1,18 +1,23 @@
+import React, { useState } from 'react';
 import img from '../Images/img.png';
 
 function WhatsappMessage() {
+    const [clickCount, setClickCount] = useState(0);
     const message = encodeURIComponent("Hey Rob!\nI want to know more about the Life Coaching you offer.");
     const phoneNumber = '+27826897696';
     const waURL = `https://wa.me/${phoneNumber}?text=${message}`;
+    const url = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
 
     function handleRedirectMessage() {
-        const url = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
-
-        const random = Math.floor((Math.random(0,100) * 100) );
-        if (random >= 65)
-            window.location.href = url;
-        else
-            window.location.href = waURL;
+        setClickCount(prevCount => {
+            const newCount = prevCount + 1;
+            if (newCount % 4 === 0) {
+                window.open(url, '_blank');
+            } else {
+                window.open(waURL, '_blank');
+            }
+            return newCount;
+        });
     }
 
     return (
